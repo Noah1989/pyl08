85328bef44ee7cd0c14a97d2cd5801ba821ca73d
Stefan Noack <stefan@noack-software.de>
2014-04-07 00:39:41 +0200

    made reset work via CBUS GPIO

--- pyl08.py
+++ pyl08.py
@@ -4,6 +4,7 @@ from ctypes import byref
 USB_PID_LIST.append(0x6015)
 BITMODE_CBUS = 0x20
 
+
 dev = SerialDevice()
 
 dev.baudrate = 46875
@@ -11,7 +12,7 @@ dev.baudrate = 46875
 # programming voltage
 dev.rts = 1
 
-#reset (does not work yet...)
-dev.ftdi_fn.ftdi_set_bitmode(byref(dev.ctx), 0x00, BITMODE_CBUS)
+# reset
+dev.ftdi_fn.ftdi_set_bitmode(0x40, BITMODE_CBUS)
 
 dev.close()
